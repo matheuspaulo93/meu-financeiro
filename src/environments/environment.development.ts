90b64959b7caa77b43a177932752429c74c29a58
Matheus Paulo Costa da Silva <matheus.paulo93@gmail.com>
2026-09-11 17:05:12 -0300

    Colocando o menu cartões de volta

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDFC9X94qBScmeiTaYjJlxdf-3U28GDclY',
+    authDomain: 'meu-financeiro-e9d35.firebaseapp.com',
+    projectId: 'meu-financeiro-e9d35',
+    storageBucket: 'meu-financeiro-e9d35.firebasestorage.app',
+    messagingSenderId: '45063439294',
+    appId: '1:45063439294:web:8d0aa493a8f9a63a192fef',
+  },
+};
+
